d3c4b5dd0041a537dbca24d10ea84b9eb04d7e18
Arecsu <alejandro9r@gmail.com>
2021-12-21 03:16:21 -0300

    new blog

---
+++ js/theme-script.js
@@ -0,0 +1,66 @@
+var storedTheme =
+   localStorage.getItem('theme') ||
+   (window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light');
+if (storedTheme) {
+   document.documentElement.setAttribute('data-theme', storedTheme);
+}
+
+var color_background = getComputedStyle(
+   document.documentElement
+).getPropertyValue('--bg-color-1');
+
+document.addEventListener('DOMContentLoaded', function () {
+   document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute('content', color_background);
+   document
+      .querySelector('meta[name="apple-mobile-web-app-status-bar-style"]')
+      .setAttribute('content', color_background);
+});
+
+// dark theme
+
+const toggle = document.getElementsByClassName('theme-toggle');
+
+function theme_toggle() {
+   var currentTheme = document.documentElement.getAttribute('data-theme');
+   var targetTheme = 'light';
+
+   if (currentTheme === 'light') {
+      targetTheme = 'dark';
+   }
+
+   document.documentElement.setAttribute('data-theme', targetTheme);
+   localStorage.setItem('theme', targetTheme);
+
+   var color_background = getComputedStyle(
+      document.documentElement
+   ).getPropertyValue('--bg-color-1');
+
+   document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute('content', color_background);
+   document
+      .querySelector('meta[name="apple-mobile-web-app-status-bar-style"]')
+      .setAttribute('content', color_background);
+}
+
+Array.from(toggle).forEach((element) => {
+   element.addEventListener('click', () => {
+      theme_toggle();
+   });
+});
+
+document.addEventListener('keypress', function onPress(event) {
+   source = event.target;
+   exclude = ['input', 'textarea'];
+
+   if (
+      (event.key === 'd' || event.key === 'D') &&
+      exclude.indexOf(source.tagName.toLowerCase()) === -1
+   ) {
+      theme_toggle();
+   }
+});
