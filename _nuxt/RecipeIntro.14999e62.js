96f555218ee35f03e00dd91ef50928232c650031
Arecsu <alejandro9r@gmail.com>
2023-08-16 02:33:02 -0300

    Updates

--- _nuxt/RecipeIntro.14999e62.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper.c27b6911.js";import{o,c as _,k as c}from"./entry.693d5503.js";const n={},r={class:"recipe-intro"};function s(e,a){return o(),_("div",r,[c(e.$slots,"default",{},void 0,!0)])}const f=t(n,[["render",s],["__scopeId","data-v-d86fa855"]]);export{f as default};
