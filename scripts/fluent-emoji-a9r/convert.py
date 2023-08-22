e0704850e13bc3e292ccf832b1e1d930c1ea79e2
Arecsu <alejandro9r@gmail.com>
2023-08-21 23:42:52 -0300

    Fluent Emoji: added Default skin tone to file structure and a progress bar

--- scripts/fluent-emoji-a9r/convert.py
+++ scripts/fluent-emoji-a9r/convert.py
@@ -1,6 +1,7 @@
 import os
 import json
 import shutil
+from pip._vendor.rich import progress
 
 folder_path = './fluentui-emoji/assets'
 json_file_name = "metadata.json"
@@ -25,12 +26,14 @@ skintones_with_modifiers = {
   'Dark':           '1f3ff'
 }
 
-# folders inside each emoji
-# 3D     = Fluent Emoji 3D   ==> PNG 256x256
-# Color  = Fluent Emoji 3D   ==> SVG
-# Flat   = Fluent Emoji Flat ==> SVG
-# emojiTypes = ['3D', 'Color', 'Flat']
+'''
+folders inside each emoji
+3D     = Fluent Emoji 3D   ==> PNG 256x256
+Color  = Fluent Emoji 3D   ==> SVG
+Flat   = Fluent Emoji Flat ==> SVG
+'''
 # skipping 3D because I want to process the format with another app
+# emojiTypes = ['3D', 'Color', 'Flat']
 emojiTypes = ['Color', 'Flat']
 
 # Get a list of subfolders in the specified directory
@@ -53,9 +56,7 @@ def create_unicode_folder(unicode_value, srcPath, json_srcPath):
   # shutil.copytree(srcPath, new_path)
   # shutil.copy(json_srcPath, unicode_folder_name)
 
-
-# Iterate through the subfolders and rename them
-for folder in subfolders:
+for folder in progress.track(subfolders):
     emoji_srcPath = os.path.join(folder_path, folder)
     json_file_path = os.path.join(emoji_srcPath, json_file_name)
     
@@ -63,6 +64,7 @@ for folder in subfolders:
       data = json.load(json_file)
 
     emoji_character = data["glyph"]
+    # print(f'{emoji_character}')
     emoji_unicode = data["unicode"]
     hasUnicodeSkinTones = "unicodeSkintones" in data
 
@@ -74,39 +76,13 @@ for folder in subfolders:
             if skintones_with_modifiers[key] in emoji_with_skintone_unicode:
               emoji_with_skintone_srcPath = os.path.join(emoji_srcPath, key)
               create_unicode_folder(emoji_with_skintone_unicode, emoji_with_skintone_srcPath, json_file_path)
-              # print(f'{key} {skintones_with_modifiers[key]} => {unicode_skin_tone}')
+      # handle Default skintone
+      emoji_default_skintone_srcPath = os.path.join(emoji_srcPath, 'Default')
+      create_unicode_folder(emoji_unicode, emoji_default_skintone_srcPath, json_file_path)
       continue
     
     create_unicode_folder(emoji_unicode, emoji_srcPath, json_file_path)
 
-
-    # if len(data["unicode"]) > 6:
-      # print("Glyph:", glyph_value)
-      # print(data["unicode"].replace(' ', '-'))
-      # if hasUnicodeSkinTones:
-        # print("unicodeSkintones:", data["unicodeSkintones"])
-        # for skintone in skintones:
-            # print('mas que 6')
-          # check_path = os.path.join(emoji_parent_path, skintone)
-          # if os.path.exists(check_path) == False:
-            # print('xddddddd')
-
-#     new_name = f'${hex(ord(folder))}'
-#     old_path = os.path.join(folder_path, folder)
-#     new_path = os.path.join(folder_path, new_name)
-    
-#     # Check if the new name already exists, and if so, modify it to avoid conflicts
-#     count = 1
-#     while os.path.exists(new_path):
-#         new_name = f'{folder}-{folder}-{count}'
-#         new_path = os.path.join(folder_path, new_name)
-#         count += 1
-
-#     # Rename the folder
-#     os.rename(old_path, new_path)
-#     print(f'Renamed "{folder}" to "{new_name}"')
-
-# print('Folder renaming completed.')
 
 ''' javascript
 emoji = "🧏‍♀️" // corresponds to  1f469-200d-2695-fe0f
