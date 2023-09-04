4f3b76889fa3b955e9e0f2453401e74f5b5b742c
Arecsu <alejandro9r@gmail.com>
2023-09-04 11:40:24 -0300

    alejandro9r.xyz homepage to this project

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,61 @@
 <template>
-   <div>
-   </div>
+  <div>
+    <p>un gusto <ESColor e="🤗" /> soy Alejandro</p>
+    <br />
+    <p>hago <a href="https://martyr.shop">martyr⁠—</a></p>
+    <p>y estoy en otros proyectos con gente alrededor del mundo</p>
+    <br />
+    <p>exploro la intersección entre la ingeniería de software y las artes visuales</p>
+    <br />
+    <p>fuera del trabajo, también:</p>
+    <ul class="list-type-1">
+      <li>me gusta leer y aprender cosas nuevas <ESFlat e="📖" /></li>
+      <li>disfruto mucho mi tiempo con familia y amigos</li>
+      <li>exploro nuevas comidas en la cocina. <NuxtLink to="/recetas">Aquí</NuxtLink> tengo algunos hallazgos.</li>
+      <li>conecto mi cuerpo y mente con Yoga</li>
+      <li>intento ser mas fuerte con calistenia</li>
+      <li>y escuchar música es <a href="https://www.last.fm/user/Alejandro9R" target="_blank">parte de mi día a día</a></li>
+    </ul>
+    <br />
+    <p>
+      — desde Tucumán, Argentina
+      <Icon name="twemoji:flag-argentina" style="vertical-align: -0.15em; margin-left: 0.2em" />
+    </p>
+    <!-- <p><ESFlat e="🌎" /> desde Tucumán, Argentina </p> -->
+    <br />
+    <p>
+      <!-- <a href="https://www.youtube.com/watch?v=lGMyPamuPjk" target="_blank"><i>don't be a sellout</i></a> -->
+    </p>
+    <!-- 
+    <div id="img-wrapper">
+      <div class="img-and-background">
+        <div class="img-background"></div>
+        <img width="1685" height="1080" src="https://alejandro9r.xyz/tami.jpg" />
+      </div>
+
+      <div class="img-control">
+        <span class="small">↑ TAMI</span>
+        <span onclick="killImg()" class="small rip-img-button">↖</span>
+      </div>
+    </div>
+     -->
+    <p id="img-text-hidden" class="img-text-c">
+      <!-- <a href="https://www.youtube.com/watch?v=ACULtdKEVdY" target="_blank">???</a> -->
+    </p>
+
+    <div class="blank-space-2"></div>
+  </div>
 </template>
+
+<script setup></script>
+
+<style>
+.list-type-1 {
+   /* list-style-type: none; */
+   list-style-type: circle;
+   padding-left: 2em;
+   margin: 0;
+}
+
+</style>
+
