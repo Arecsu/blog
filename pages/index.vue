5f6743fb9f178c913e14f1d62700a5bc0b82f3d6
Arecsu <alejandro9r@gmail.com>
2023-11-02 14:47:30 -0300

    better homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,38 @@
 <template>
-  <div>
-    <p>un gusto <ESColor e="🤗" /> soy Alejandro</p>
-    <br />
-    <p>hago <a href="https://martyr.shop">martyr⁠—</a></p>
-    <p>y estoy en otros proyectos con gente alrededor del mundo</p>
-    <br />
+  <div class="base-container">
+    <p>hola! <ESColor e="🤗" /> soy Alejandro</p>
+    <p>
+      hago <a href="https://martyr.shop">martyr⁠—</a>
+      <br />
+      y estoy en otros proyectos con gente alrededor del mundo <ESFlat e="🌎" />
+    </p>
     <p>exploro la intersección entre la ingeniería de software y las artes visuales</p>
-    <br />
-    <p>fuera del trabajo, también:</p>
+    <p style="margin: 0">fuera del trabajo, también:</p>
     <ul class="list-type-1">
       <li>me gusta leer y aprender cosas nuevas <ESFlat e="📖" /></li>
       <li>disfruto mucho mi tiempo con familia y amigos</li>
-      <li>exploro nuevas comidas en la cocina. <NuxtLink to="/recetas">Aquí</NuxtLink> tengo algunos hallazgos.</li>
-      <li>conecto mi cuerpo y mente con Yoga</li>
-      <li>intento ser mas fuerte con calistenia</li>
-      <li>y escuchar música es <a href="https://www.last.fm/user/Alejandro9R" target="_blank">parte de mi día a día</a></li>
+      <li>exploro nuevas comidas en la cocina y comparto algunas <NuxtLink to="/recetas">aquí</NuxtLink></li>
+      <li>hago Yoga y entreno fuerza</li>
+      <li>
+        y escuchar música es <a href="https://www.last.fm/user/Alejandro9R" target="_blank">parte de mi día a día</a>
+      </li>
     </ul>
-    <br />
     <p>
-      — desde Tucumán, Argentina
+      desde Tucumán, Argentina
       <Icon name="twemoji:flag-argentina" style="vertical-align: -0.15em; margin-left: 0.2em" />
     </p>
-    <!-- <p><ESFlat e="🌎" /> desde Tucumán, Argentina </p> -->
-    <br />
-    <p>
-      <!-- <a href="https://www.youtube.com/watch?v=lGMyPamuPjk" target="_blank"><i>don't be a sellout</i></a> -->
-    </p>
+    <!-- <p> -->
+    <!-- <a href="https://www.youtube.com/watch?v=lGMyPamuPjk" target="_blank"><i>don't be a sellout</i></a> -->
+    <!-- </p> -->
+    <div class="spacer" />
+    <div class="text-small text-footer">
+    <!-- <div class="text-footer"> -->
+      <p style="white-space: pre">
+        <a href="https://github.com/Arecsu/" target="_blank">github</a>{{ "  //  "
+        }}<a href="https://instagram.com/alejandro9r" target="_blank">instagram</a>
+        <br/><a href="mailto:alejandro@martyr.shop">email</a>: alejandro@martyr.shop
+      </p>
+    </div>
     <!-- 
     <div id="img-wrapper">
       <div class="img-and-background">
@@ -42,20 +49,49 @@
     <p id="img-text-hidden" class="img-text-c">
       <!-- <a href="https://www.youtube.com/watch?v=ACULtdKEVdY" target="_blank">???</a> -->
     </p>
-
-    <div class="blank-space-2"></div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+useHead({
+  title: "",
+})
+</script>
 
-<style>
-.list-type-1 {
-   /* list-style-type: none; */
-   list-style-type: circle;
-   padding-left: 2em;
-   margin: 0;
+<style lang="scss">
+.base-container {
+  display: flex;
+  flex-direction: column;
+  // margin-bottom: 1em;
+  // margin: 3em;
+  margin: 2.2vw 2.5vw 3vw 2.2vw;
+  p,
+  ul {
+    &:not(:last-child) {
+      margin: 0;
+      margin-bottom: 1.6em;
+    }
+  }
 }
 
-</style>
+.spacer {
+  height: 2em;
+}
 
+.list-type-1 {
+  list-style-type: circle;
+  padding-left: 2em;
+  margin: 0;
+}
+
+.text-footer {
+  margin-top: auto;
+}
+
+.text-small {
+  font-size: 0.85em;
+  a {
+    text-underline-offset: 0.12em;
+  }
+}
+</style>
